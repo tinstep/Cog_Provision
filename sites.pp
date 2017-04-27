ed1d388177c57271fc67d48ba6ed59d1053ca851
tinstep <carrick_@fastmail.fm>
2017-04-27 20:23:44 +1000

    Alignment

--- sites.pp
+++ sites.pp
@@ -20,10 +20,10 @@ group { 'cognosgrp2':
 
 # password is same as user
 user { 'cognos1':
- 			  ensure           => 'present',
+ 			ensure           => 'present',
       home             => '/home/cognos1',
-      comment           => 'service account',
-      groups            => 'cognosgrp1',
+      comment          => 'service account',
+      groups           => 'cognosgrp1',
       password         => '$6$salt$lG0TId1khdHnoxmnhe7jl5/zvlFFUbknSf3ZyD2PuJ5rhKiEjI3L7KujcNjBhBdjapi8hujYqG/I4ajVbr2Pr1',
       password_max_age => '99999',
       password_min_age => '0',
@@ -34,10 +34,10 @@ user { 'cognos1':
 
 # pssword is same as user
 user { 'cognos2':
- 			  ensure           => 'present',
+ 			ensure           => 'present',
       home             => '/home/cognos2',
-      comment           => 'service account',
-      groups            => 'cognosgrp2',
+      comment          => 'service account',
+      groups           => 'cognosgrp2',
       password         => '$6$salt$Kr1qMlv.v5VSwZtKZhNdDYcrmeEsEgUoBctr1Zk.T9XYEWfo0DIeBUrwbRXCq1qXYCQ6ueRXA4rbw3RA.2WEg/',
       password_max_age => '99999',
       password_min_age => '0',
@@ -71,10 +71,10 @@ package { [
 }
 
 
-$ssh_service = $operatingsystem ? { 
-    /CentOS|RedHat/  => 'sshd', 
+$ssh_service = $operatingsystem ? {
+    /CentOS|RedHat/  => 'sshd',
     default          => 'ssh' ,
-} 
+}
 
 package { 'openssh-server':
     ensure => 'latest',
@@ -110,4 +110,3 @@ package { [
   ]:
   ensure => 'installed',
 }
-
