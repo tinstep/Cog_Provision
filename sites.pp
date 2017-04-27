49de448530a327810285b2f1fa33df808b1beefb
tinstep <spamme@fastmail.fm>
2017-04-27 12:48:07 +1000

    Update sites.pp

--- sites.pp
+++ sites.pp
@@ -1,3 +1,12 @@
+case $facts['os']['name'] {
+  'Solaris':           { include role::solaris } # Apply the solaris class
+  'RedHat', 'CentOS':  { include role::redhat  } # Apply the redhat class
+  /^(Debian|Ubuntu)$/: { include role::debian  } # Apply the debian class
+  default:             { include role::generic } # Apply the generic class
+}
+
+
+
 
 group { 'cognosgrp1':
   			  ensure => 'present',
